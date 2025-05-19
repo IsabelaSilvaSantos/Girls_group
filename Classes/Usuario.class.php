be372a674fd4cf655d5eccb654d7dd3010349f41
Amanda <amandagisele456@gmail.com>
2025-05-19 14:55:34 -0400

    arrumei o banco de dados

--- Classes/Usuario.class.php
+++ Classes/Usuario.class.php
@@ -1,29 +1,43 @@
 <?php
 
-class Usuario extends CRUD {
-    protected $table = "usuario";
+class Usuario extends CRUD{
+    protected $table = "cadastro_usario";
     private $nome;
     private $email;
+    
+    private $papel;
     private $senha;
 
-public function getnome() {
-    return $this->nome;
-}
-public function setemail() {
-    return $this->email;
-}
-public function getsenha() {
-    return $this->senha;
-}
+    public function getnome()
+    {
+        $this->nome;
+    }
+    public function setemail()
+    {
+        $this->email;
+    }
+    public function setpapel()
+    {
+        $this->papel;
+    }
+    public function getsenha()
 
-public function add() {
-$sql = "INSERT INTO $this->table(nome, email, senha) VALUES (:nome, email, senha)";
-$stmt = $this->db->prepare($sql);
-$stmt->bindParam("nome", $this->nome, $this->nome);
-$stmt->bindParam("email", $this->email);
-$stmt->bindParam("senha", $this->senha);
-return $stmt->execute();
-}
+    {
+        $this->senha;
+    }
+
+
+
+    {  
+    public function add()
+        $sql = "INSERT INTO $this->table(nome, email, senha) VALUES (:nome, email, senha)";
+        $stmt = $this->db->prepare($sql);
+        $stmt->bindParam("nome", $this->nome, $this->nome);
+        $stmt->bindParam("email", $this->email);
+        $stmt->bindParam("papel", $this->papel);
+        $stmt->bindParam("senha", $this->senha);
+        return $stmt->execute();
+    }
 
 
 
