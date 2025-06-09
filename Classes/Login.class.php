611b4e5b23daf57226d05e0174bf057b6590f981
KamillaG <kamillamidia@icloud.com>
2025-06-09 12:13:34 -0400

    Fiz dbLogin, gerLogin, e Login.class.php para a Kamilla

---
+++ Classes/Login.class.php
@@ -0,0 +1,34 @@
+<?php
+
+class Login extends CRUD{
+    protected $table = "Login";
+    private $usuario;
+    private $senha;
+
+    
+    public function getusuario(){
+       return $this->usuario;
+    }
+    public function setusuario($usuario){
+        $this->usuario = $usuario;
+    }
+    public function getsenha(){
+        return $this->senha;
+    }
+    public function setsenha($senha){
+        $this->senha = $senha;
+    }
+
+
+    public function add() {  
+        $sql = "INSERT INTO $this->table(usuario, senha) VALUES (:usuario, :senha)";
+        $stmt = $this->db->prepare($sql);
+        $stmt->bindParam(":usuario", $this->usuario, PDO::PARAM_STR);
+        $stmt->bindParam(":senha", $this->senha, PDO::PARAM_STR);
+        return $stmt->execute();
+    }
+
+    public function update(string $campo, int $id){
+
+    }
+}
