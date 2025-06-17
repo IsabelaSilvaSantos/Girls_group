8d42cbeefd5adc619bba62b8050481954b71f3b4
KamillaKempim <kamillamidia@icloud.com>
2025-06-17 16:25:48 -0400

    Arrumei o update do Login.class.php, para a kamilla

--- Classes/Login.class.php
+++ Classes/Login.class.php
@@ -22,11 +22,20 @@ class Login extends CRUD{
 
     public function add() {  
         $senhaComHash = password_hash($this->senha, PASSWORD_DEFAULT);
-        $sql = "INSERT INTO $this->table (usuario, senha) VALUES 
-(:usuario, :senha)";
+        $sql = "INSERT INTO $this->table (usuario, senha) VALUES (:usuario, :senha)";
         $stmt = $this->db->prepare($sql);
         $stmt->bindParam(":usuario", $this->usuario, PDO::PARAM_STR);
         $stmt->bindParam(":senha", $this->senha, PDO::PARAM_STR);
         return $stmt->execute();
+
     }
-    }
+        public function update(string $campo, int $id){
+            $sql = "UPDATE $this->table SET nome=:usuario, :senha)  WHERE $campo=:usuario, :senha)";
+            $stmt = $this->db->prepare($sql);
+            $stmt->bindParam(":usuario", $id, PDO::PARAM_INT);
+            $stmt->bindParam(":senha", $id, PDO::PARAM_INT);
+            return $stmt->execute();
+        }
+        }
+        
+    
