ce9e62da2f837a53a1bb2df57a89a35faf4b5e5f
IsabelaSilva <isasilvaglo39@gmail.com>
2025-07-04 08:04:48 -0400

    correção roda pé

--- _parts/_footer.php
+++ _parts/_footer.php
@@ -1,13 +1,14 @@
-<div class="r-linha" style="background-color: rgb(0, 0, 0);">
-
-    <div class="r-linha-item" style="background-color: rgb(255, 255, 255);;">
+<div class="r-linha">
+    <div class="r-linha-item">
+        <img src="Images/FazendaBelaLogo.png" alt="Logo da Empresa">
         <p>Contatos:</p>
-        <p><i class="bi bi-telephone"></i> (69) 9999-9999</p>
+        <p><i class="bi bi-telephone"></i> (69) 98406-0305</p>
     </div>
-    <div class="r-linha-item" style="background-color: rgb(196, 210, 219);">
-        <a href="#"><i class="bi bi-facebook"></i></a>
-        <a href="#"><i class="bi bi-instagram"></i></a>
-        <a href="#"><i class="bi bi-whatsapp"></i></a>
+    <div class="r-linha-item rede-social">
+        <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+        <a href="https://www.instagram.com/_isabela_13g/" class="instagram" target="_blank"><i
+                class="bi bi-instagram"></i></a>
+        <a href="https://wa.me/5569984060305" class="whatsapp" target="_blank"><i class="bi bi-whatsapp"></i></a>
     </div>
 </div>
-<p>&copy; 2025, Todos os direitos reservados</p>
+<p>&copy; 2025 Fazenda Bela, Todos os direitos reservados</p>
