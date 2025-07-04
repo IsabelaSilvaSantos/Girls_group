1db444e04e7655309387cd757ca8bc33a7544cd5
IsabelaSilvaSantos <isasilvaglo39@gmail.com>
2025-07-03 22:07:53 -0400

    mudei o menu

--- _parts/_menu.php
+++ _parts/_menu.php
@@ -1,24 +1,29 @@
-<nav class="navbar navbar-expand-lg sticky-top" data-bs-theme="dark">
-    <div class="container" style="background-color: rgb(0, 0, 0);">
-        <a class="navbar-brand text-white" href="#">Produtos</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-            data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
-            aria-label="Toggle navigation">
+<nav class="navbar navbar-expand-lg bg-body-tertiary sticky-top">
+    <div class="container">
+        <img src=" " alt="Logo da empresa">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a href="index.php" class="nav-link">Home</a>
+                <li class="navbar-item">
+                    <a href="#" class="nav-link">Home</a>
                 </li>
-                <li class="nav-item">
-                    <a href="animais.php" class="nav-link">Empresa</a>
+                <li class="navbar-item">
+                    <a href="#" class="nav-link">Página Principal</a>
                 </li>
-                <li class="nav-item">
-                    <a href="categorias.php" class="nav-link">Categoria</a>
+                <li class="navbar-item">
+                    <a href="#" class="nav-link">Usuário</a>
                 </li>
-                <li class="nav-item">
-                    <a href="produtos.php" class="nav-link active">Produto</a>
+                <li class="navbar-item">
+                    <a href="#" class="nav-link">Cliente</a>
+                </li>
+                <li class="navbar-item">
+                    <a href="#" class="nav-link">Produtos</a>
+                </li>
+                <li class="navbar-item">
+                    <a href="#" class="nav-link">Contato</a>
                 </li>
             </ul>
         </div>
