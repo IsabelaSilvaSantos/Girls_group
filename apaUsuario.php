1b8da8f2c564ddf8a5a6ebc43bf90aa31fdca1df
emanuelester <emanuelester18032004@gmail.com>
2025-07-04 09:11:39 -0400

    Terminei com Usuario, e arrumei o codigo dos botões

--- apaUsuario.php
+++ apaUsuario.php
@@ -52,7 +52,7 @@
                             </form>
 
                             <form action="<?php echo htmlspecialchars("dbUsuario.php") ?>" method="post" class="d-flex">
-                                <input type="hidden" name="id" value="<?php echo $$Usuario->id ?>">
+                                <input type="hidden" name="id" value="<?php echo $Usuario->id ?>">
                                 <button name="btnDeletar" class="btn btn-outline-danger btn-sm" type="submit"
                                     onclick="return confirm('Tem certeza que deseja deletar o Usuario?');"><i
                                     class="bi bi-trash"></i>
--- dbUsuario.php
+++ dbUsuario.php
@@ -1,17 +1,18 @@
 <?php
-if(filter_has_var(INPUT_POST,"btnGravar")):
     spl_autoload_register(function($class){
       require_once "classes/{$class}.class.php";
     });
     //Criando uma intância da classe Usuario
     $Usuario = new Usuario();
+if(filter_has_var(INPUT_POST,"btnGravar")):
     $Usuario->setnome(filter_input(INPUT_POST, "usuario", FILTER_SANITIZE_STRING));
     $Usuario->setemail(filter_input(INPUT_POST, "email", FILTER_SANITIZE_STRING));
     $Usuario->setsenha(filter_input(INPUT_POST, "senha", FILTER_SANITIZE_STRING));
     $Usuario->setpapel(filter_input(INPUT_POST, "papel", FILTER_SANITIZE_STRING));
-    $idUsuario = filter_input(INPUT_POST, 'id');
+    $id = filter_input(INPUT_POST, 'id');
+    
 
-    if (empty($idUsuario)):
+    if (empty($id)):
         //Tentar adicionar e exibe a mensagem ao usuário
         if($Usuario->add()):
             echo "<script>window.alert('Cadastrado com sucesso.');windows.location.href=gerUsuario.php;</script>";
@@ -19,7 +20,7 @@ if(filter_has_var(INPUT_POST,"btnGravar")):
             echo "<script>window.alert('Erro ao cadastrar.');window.open(document.referrer,'_self');</script>";
         endif;
     else:
-        if ($Usuario->update('id', $idUsuario)):
+        if ($Usuario->update('id', $id)):
             echo "<script> window.alert('Raça alterada com sucesso.');window.location.href='apaProdutos.php'; </script>";
         else:
             echo "<script> window.alert('Erro ao alterar o produto.');window.open(document.referrer, '_self'); </script>";
@@ -27,8 +28,8 @@ if(filter_has_var(INPUT_POST,"btnGravar")):
     endif;
 
 elseif (filter_has_var(INPUT_POST, "btnDeletar")):
-    $idUsuario = intval(filter_input(INPUT_POST, "id"));
-    if ($Usuario->delete("id", $idUsuario)):
+    $id = intval(filter_input(INPUT_POST, "id"));
+    if ($Usuario->delete("id", $id)):
         header("location:apaUsuario.php");
     else:
         echo "<script>window.alert('Erro ao Excluir');window(document.referrer,'_self');</script>";
