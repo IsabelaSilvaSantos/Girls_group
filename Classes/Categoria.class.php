28e0e9e6ea1b1c2d9a2cceb9326a754f70cd58d8
IsabelaS <isasilvaglo39@gmail.com>
2025-11-07 14:00:22 -0400

    exclui clientes

--- Classes/Categoria.class.php
+++ Classes/Categoria.class.php
@@ -1,9 +1,15 @@
 <?php
+// Classe Categoria gerencia as regras de negócio e a interação com o banco de dados para o cadastro de categorias.
 class Categoria extends CRUD
 {
-    protected $table = "categoria";
+    // A propriedade protected $table define o nome da tabela no banco de dados com a qual a classe irá operar.
+    protected $table = "categoria"; 
+    // As propriedades privadas representam as colunas da tabela.
+    // O uso do modificador 'private' garante o Encapsulamento, protegendo o acesso direto aos dados.
     private $id_categoria;
     private $nome_categoria;
+
+    // Métodos Getters e Setters são Acessos e Modificações Controlados por Dados
 
     public function getid_categoria()
     {
@@ -25,6 +31,7 @@ class Categoria extends CRUD
         $this->nome_categoria = $nome_categoria;
     }
 
+    // Método responsável por buscar uma única categoria no banco de dados usando o ID.
     public function getCategoriaById(int $id)
     {
         $sql = "SELECT id_categoria, nome_categoria FROM {$this->table} WHERE id_categoria = :id";
@@ -34,6 +41,7 @@ class Categoria extends CRUD
         return $stmt->fetch(PDO::FETCH_OBJ);
     }
 
+    // Permite que o sistema cadastre novas categorias para os produtos do segmento escolhido.
     public function add()
     {
         $sql = "INSERT INTO {$this->table} (nome_categoria) VALUES (:nome_categoria)";
@@ -42,6 +50,7 @@ class Categoria extends CRUD
         return $stmt->execute();
     }
 
+    // Permite que o administrador edite o nome de uma categoria existente.
     public function update(string $campo, int $id)
     {
         $sql = "UPDATE {$this->table} SET nome_categoria = :nome_categoria WHERE id_categoria = :id_categoria";
